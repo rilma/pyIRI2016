2e99f47d572f3adf05097abb802f069fab849376
Ronald Ilma <rri5@cornell.edu>
2022-01-15 21:59:43 +0000

    drafting retrieve test case

--- pyiri2016/api/update.py
+++ pyiri2016/api/update.py
@@ -1,15 +1,23 @@
+import re
 from typing import List
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-import re
+import wget
 
-def _get_links(url: str) -> List[str]:
-    request = Request(url)
-    html_page = urlopen(request)
+def _get_links(url: str, pattern: str=r'.') -> List[str]:
+    html_page = urlopen(Request(url))
     soup = BeautifulSoup(html_page, "html.parser")
-    links = [link.get('href') for link in soup.findAll('a')]
+    links = [link.get('href') for link in soup.findAll('a') if re.match(pattern, link.get('href'), re.DOTALL)]
     return links
 
-def retrieve(url: str) -> None:
-    links = _get_links(url)
+def retrieve(url: str, pattern: str, directory: str) -> None:
+    links = _get_links(url, pattern=pattern)
+    # from pprint import pprint
+    print(links)
+    print(len(links))
+    for link in links:
+        filename = wget.download(f'{url}{link}', out=directory, bar=wget.bar_thermometer)
+        print('\n', filename)
+        break
     return None
+    # re.match(r'[a-z0-9_]+.[a-z]+'
