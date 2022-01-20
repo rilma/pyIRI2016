2df20639e96a696328feb66c6a4cf7723a8f8a3c
Ronald I <rri5@cornell.edu>
2022-01-20 19:05:07 +0100

    Update update.py

--- pyiri2016/api/update.py
+++ pyiri2016/api/update.py
@@ -1,16 +1,6 @@
-import re
-from typing import List
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
 import wget
 import tarfile
 import os
-
-def _get_links(url: str, pattern: str=r'.') -> List[str]:
-    html_page = urlopen(Request(url))
-    soup = BeautifulSoup(html_page, "html.parser")
-    links = [link.get('href') for link in soup.findAll('a') if re.match(pattern, link.get('href'), re.DOTALL)]
-    return links
 
 def retrieve(url: str, filename: str, directory: str) -> None:
     retrieved_fullpath = wget.download(f'{url}/{filename}', out=directory, bar=wget.bar_thermometer)
