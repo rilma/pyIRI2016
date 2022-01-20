c9a746d6db9f1f47f2d0a16e4dd195b7cc6bad66
Ronald Ilma <rri5@cornell.edu>
2022-01-20 13:54:59 +0000

    final version of update.retrieve method

--- pyiri2016/api/update.py
+++ pyiri2016/api/update.py
@@ -12,20 +12,10 @@ def _get_links(url: str, pattern: str=r'.') -> List[str]:
     links = [link.get('href') for link in soup.findAll('a') if re.match(pattern, link.get('href'), re.DOTALL)]
     return links
 
-def retrieve_code(url: str, compressed_filename: str, directory: str) -> None:
-    retrieved_fullpath = wget.download(f'{url}/{compressed_filename}', out=directory, bar=wget.bar_thermometer)
-    with tarfile.open(retrieved_fullpath) as tar:
-        tar.extractall(path=directory)
-    os.remove(retrieved_fullpath)
-    
-# def retrieve(url: str, compressed, pattern: str, directory: str) -> None:
-#     links = _get_links(url, pattern=pattern)
-#     # from pprint import pprint
-#     print(links)
-#     print(len(links))
-#     for link in links:
-#         filename = wget.download(f'{url}{link}', out=directory, bar=wget.bar_thermometer)
-#         print('\n', filename)
-#         # break
-#     return None
-#     # re.match(r'[a-z0-9_]+.[a-z]+'
+def retrieve(url: str, filename: str, directory: str) -> None:
+    retrieved_fullpath = wget.download(f'{url}/{filename}', out=directory, bar=wget.bar_thermometer)
+    if tarfile.is_tarfile(retrieved_fullpath):
+        with tarfile.open(retrieved_fullpath) as tar:
+            tar.extractall(path=directory)
+        os.remove(retrieved_fullpath)
+    return None
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,26 +2,26 @@ import tempfile
 from pyiri2016.api import update
 from unittest import TestCase
 from simple_settings import LazySettings
+from parameterized import parameterized
+
+SETTINGS = LazySettings('settings.settings')
 
 class TestApiUpdate(TestCase):        
 
     @classmethod
     def setUpClass(cls):
-        cls.settings = LazySettings('settings.settings')
         cls.temporary_directory = tempfile.TemporaryDirectory()
 
     @classmethod
     def tearDownClass(cls):
         cls.temporary_directory.cleanup()
 
-    def test_retrieve_code(self):
-        result = update.retrieve_code(url=self.settings.FORTRAN_CODE_URL, compressed_filename=self.settings.FORTRAN_CODE_COMPRESSED_FILE, directory=self.temporary_directory.name)
-        # import os; os.system(f'ls -lah {self.temporary_directory.name}')
+    @parameterized.expand([
+        (SETTINGS.FORTRAN_CODE_URL, SETTINGS.FORTRAN_CODE_COMPRESSED_FILE),
+        (SETTINGS.COMMON_FILES_URL, SETTINGS.COMMON_FILES_COMPRESSED_FILE),
+        (SETTINGS.INDICES_URL, SETTINGS.INDICES_FILES[0]),
+        (SETTINGS.INDICES_URL, SETTINGS.INDICES_FILES[1]),
+    ])
+    def test_retrieve(self, url: str, filename: str):
+        result = update.retrieve(url, filename, directory=self.temporary_directory.name)
         self.assertIsNone(result)
-
-    # def test_retrieve(self):
-    #     settings = LazySettings('settings.settings')
-    #     result = update.retrieve(settings.FORTRAN_CODE_URL, settings.FILENAME_REGEX_PATTERN, self.temporary_directory.name)
-    #     import os; os.system(f'ls -lah {self.temporary_directory.name}')
-    #     self.assertIsNotNone(result)
-    
