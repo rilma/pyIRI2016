2872f4187b12a234a21b56f7278adc865a2753eb
Ronald I <14822065+rilma@users.noreply.github.com>
2026-02-21 20:07:49 +0000

    ruff fix

--- examples/example02.py
+++ examples/example02.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     def main1():
 
-        iri2016Obj = IRI2016Profile(option=1, verbose=True)
+        IRI2016Profile(option=1, verbose=True)
 
     # Height Profile Example
     main1()
